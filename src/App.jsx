5acfc459f4435ab1dbc0acdd2b3649d165975a1f
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-02-13 00:27:49 -0800

    Init commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>
+        Blueprint Thingy
+      </h1>
+      <h3>
+        coming soon™
+      </h3>
+    </>
+  )
+}
+
+export default App
